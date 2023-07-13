9d0eb67557f48a5bdbc99e00639736f5e59f5ac8
jennipher <voliduser@gmail.com>
2023-07-13 04:00:45 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+"Hello!".toUpperCase(); //'HELLO!'
+
+function whisper(string) {
+    return string.toLowerCase();}
+"Hello!".toLowerCase(); //'hello!'
+
+function logShout(string){
+    console.log(string.toUpperCase("Hello!"));
+}
+
+function logWhisper(string){
+console.log(string.toLowerCase("hello!"));
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    var cantAnswer = "I can't hear you!";
+    var yesAnswer = "YES INDEED!";
+if (string.toLowerCase(string) ===string){return cantAnswer;}
+   else if (string.toUpperCase(string) ===string) {return yesAnswer;}
+   else if (string ===string) {return "I would love to!"}
+}
+
+
+
+
+
+
+
+
+
+
+
+
